e0c02a4f353436f459d4764f98370fab80d80c9d
maxlayn1 <maxlayn1@gmail.com>
2026-02-05 14:31:44 -0600

    current theory is that keeping chunk size low reduces CPU spikes

--- src/main.rs
+++ src/main.rs
@@ -71,10 +71,10 @@ Decision:";
     println!("Generating response...\n");
 
     // --- tunable knobs ---!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    let chunk_size: usize = 16;                                     // tokens per chunk (increased to 16 from 4 for less overhead since fewer decode calls)
+    let chunk_size: usize = 2;                                     // tokens per chunk (increased to 16 from 4 for less overhead since fewer decode calls)
     let chunk_delay = Duration::from_millis(10000);       // pause between chunks
-    let gen_delay = Duration::from_millis(100);           // to throttle generation for reduced CPU load
-    let max_tokens = 20;                                       //since only expecting 'KEEP' or 'DELETE' no need for many tokens
+    let gen_delay = Duration::from_millis(0);           // to throttle generation for reduced CPU load
+    let max_tokens = 20;                                       // since only expecting 'KEEP' or 'DELETE' no need for many tokens
     // ----------------------
 
     let mut batch = LlamaBatch::new(64, 1);
